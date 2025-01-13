0d70151ca3bffadba2271b2ce07aedd837ffae51
Katie Collins <katiecollins@Katies-Air-7.cable.rcn.com>
2025-01-13 15:33:33 -0500

    plot formatting. updated update schedule

--- utils.py
+++ utils.py
@@ -3,60 +3,86 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-
-def plot_results(params_dict, social_learner_freqs, ai_bias_means, change_points, ai_adaptation, learner_adaptation, social_learner_adaptation):
-    dname=f"figures/{params_dict['sim_name']}/"
-    os.makedirs(dname, exist_ok=1)
+def plot_results(params_dict, social_learner_freqs, ai_bias_means, change_points, ai_adaptation, learner_adaptation, social_learner_adaptation,
+                 axis_font_size=18, axis_tick_size=10):
+    # Set global font to serif
+    plt.rcParams['font.family'] = 'serif'
     
-    inds=np.where(change_points)
+    dname = f"figures/{params_dict['sim_name']}/"
+    os.makedirs(dname, exist_ok=1)
+    inds = np.where(change_points)
     
     if params_dict["social_learning_mode"] and not params_dict["critical"]:
+        plt.figure()
         plt.plot(range(params_dict['n_records']), social_learner_freqs)
         plt.ylim((0,1))
         plt.scatter(inds, np.array(change_points)[inds]-1)
-        plt.ylabel("Social Learner Freq.")
+        plt.ylabel("Social Learner Freq.", fontfamily='serif', fontsize=axis_font_size)
+        plt.xlabel("Time", fontfamily='serif', fontsize=axis_font_size)
+        plt.xticks(fontfamily='serif', fontsize=axis_tick_size)
+        plt.yticks(fontfamily='serif', fontsize=axis_tick_size)
         plt.savefig(dname+f"{params_dict['sim_name']}-social_learner_freq.png")
-        plt.show()
-        
+        #plt.close()
+
+        plt.figure()
         plt.plot(range(params_dict['n_records']), social_learner_adaptation)
         plt.ylim((0,1))
-        # plt.plot([0,params_dict['n_records']],[1,1],linestyle='--')
-        plt.ylabel("Social learner adaptation")
+        plt.ylabel("Social learner adaptation", fontfamily='serif', fontsize=axis_font_size)
+        plt.xlabel("Time", fontfamily='serif', fontsize=axis_font_size)
+        plt.xticks(fontfamily='serif', fontsize=axis_tick_size)
+        plt.yticks(fontfamily='serif', fontsize=axis_tick_size)
         plt.savefig(dname+f"{params_dict['sim_name']}-social_learner_adap.png")
-        plt.show()
-    
+        #plt.close()
+
     if params_dict["social_learning_mode"] in ["both"]:
+        plt.figure()
         plt.plot(range(params_dict['n_records']), ai_bias_means)
         plt.plot([0,params_dict['n_records']],[1,1],linestyle='--')
-        plt.ylabel("AI bias")
+        plt.ylabel("AI bias", fontfamily='serif', fontsize=axis_font_size)
+        plt.xlabel("Time", fontfamily='serif', fontsize=axis_font_size)
+        plt.xticks(fontfamily='serif', fontsize=axis_tick_size)
+        plt.yticks(fontfamily='serif', fontsize=axis_tick_size)
         plt.savefig(dname+f"{params_dict['sim_name']}-ai_bias.png")
-        plt.show()
-        
+        #plt.close()
+
+        plt.figure()
         plt.plot(range(params_dict['n_records']), ai_bias_means/(ai_bias_means+1))
         plt.plot([0,params_dict['n_records']],[1,1],linestyle='--')
-        plt.ylabel("AI probability")
+        plt.ylabel("AI probability", fontfamily='serif', fontsize=axis_font_size)
+        plt.xlabel("Time", fontfamily='serif', fontsize=axis_font_size)
+        plt.xticks(fontfamily='serif', fontsize=axis_tick_size)
+        plt.yticks(fontfamily='serif', fontsize=axis_tick_size)
         plt.savefig(dname+f"{params_dict['sim_name']}-ai_prob.png")
-        plt.show()
-    if params_dict["social_learning_mode"] in ["ai", "both"]:   
+        #plt.close()
+
+    if params_dict["social_learning_mode"] in ["ai", "both"]:
+        
+        plt.figure()
         plt.plot(range(params_dict['n_records']), ai_adaptation)
         plt.ylim((0,1))
-        # plt.plot([0,params_dict['n_records']],[1,1],linestyle='--')
-        plt.ylabel("AI adaptation")
+        plt.ylabel("AI adaptation", fontfamily='serif', fontsize=axis_font_size)
+        plt.xlabel("Time", fontfamily='serif', fontsize=axis_font_size)
+        plt.xticks(fontfamily='serif', fontsize=axis_tick_size)
+        plt.yticks(fontfamily='serif', fontsize=axis_tick_size)
         plt.savefig(dname+f"{params_dict['sim_name']}-ai_adap.png")
-        plt.show()
-        
+        #plt.close()
+    
+    print('collective model understanding')
+    plt.figure()
     plt.plot(range(params_dict['n_records']), learner_adaptation)
     plt.ylim((0,1))
-    # plt.plot([0,params_dict['n_records']],[1,1],linestyle='--')
-    plt.ylabel("Population adaptation")
+    plt.ylabel("Pop world understanding", fontfamily='serif', fontsize=axis_font_size)
+    plt.xlabel("Time", fontfamily='serif', fontsize=axis_font_size)
+    plt.xticks(fontfamily='serif', fontsize=axis_tick_size, rotation=70)
+    plt.yticks(fontfamily='serif', fontsize=axis_tick_size, rotation=70)
     plt.savefig(dname+f"{params_dict['sim_name']}-population_adap.png")
+    #plt.close()
+    
+    window_size = 1
+    plt.figure()
+    averaged_adaptation = np.convolve(social_learner_adaptation, np.ones(window_size)/window_size, mode='valid')
+    plt.plot(list(range(params_dict['n_records']))[window_size-1:], averaged_adaptation)
+    plt.ylim((0,1))
+    # plt.plot([0,params_dict['n_records']],[1,1],linestyle='--')
+    plt.ylabel("Social learner adaptation")
     plt.show()
-    
-    
-    # window_size = 1
-    # averaged_adaptation = np.convolve(social_learner_adaptation, np.ones(window_size)/window_size, mode='valid')
-    # plt.plot(list(range(params_dict['n_records']))[window_size-1:], averaged_adaptation)
-    # plt.ylim((0,1))
-    # # plt.plot([0,params_dict['n_records']],[1,1],linestyle='--')
-    # plt.ylabel("Social learner adaptation")
-    # plt.show()
