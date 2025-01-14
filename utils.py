0e989db1ed1a1c607e9af4c5b396d5804eb9074c
Katie Collins <katiecollins@Katies-MacBook-Air-7.local>
2025-01-13 19:55:34 -0500

    adding back heatmap

--- utils.py
+++ utils.py
@@ -2,9 +2,12 @@ from numba import jit
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import seaborn as sns
 
-def plot_results(params_dict, social_learner_freqs, ai_bias_means, change_points, ai_adaptation, learner_adaptation, social_learner_adaptation,
-                 axis_font_size=18, axis_tick_size=10):
+def plot_results(params_dict, social_learner_freqs, ai_bias_means, change_points, ai_adaptation, learner_adaptation, 
+                 social_learner_adaptation,
+                 axis_font_size=18, axis_tick_size=10,
+                 time_thresh=-1):
     # Set global font to serif
     plt.rcParams['font.family'] = 'serif'
     
@@ -79,11 +82,41 @@ def plot_results(params_dict, social_learner_freqs, ai_bias_means, change_points
     plt.savefig(dname+f"{params_dict['sim_name']}-population_adap.pdf", dpi=300)
     #plt.close()
     
-    window_size = 1
-    plt.figure()
-    averaged_adaptation = np.convolve(social_learner_adaptation, np.ones(window_size)/window_size, mode='valid')
-    plt.plot(list(range(params_dict['n_records']))[window_size-1:], averaged_adaptation)
-    plt.ylim((0,1))
-    # plt.plot([0,params_dict['n_records']],[1,1],linestyle='--')
-    plt.ylabel("Social learner adaptation")
-    plt.show()
+    # window_size = 1
+    # plt.figure()
+    # averaged_adaptation = np.convolve(social_learner_adaptation, np.ones(window_size)/window_size, mode='valid')
+    # plt.plot(list(range(params_dict['n_records']))[window_size-1:], averaged_adaptation)
+    # plt.ylim((0,1))
+    # # plt.plot([0,params_dict['n_records']],[1,1],linestyle='--')
+    # plt.ylabel("Social learner adaptation")
+    # plt.show()
+    
+    
+def get_heatmap(params_dict, all_scores, xvals, yvals, xlabel="", ylabel="", ax_font_size=18, plt_tag="heatmap"): 
+    
+    plt.rcParams['font.family'] = 'serif'
+    
+    dname = f"figures/{params_dict['sim_name']}/"
+    os.makedirs(dname, exist_ok=1)
+    
+    
+    scores_array = np.array(all_scores)
+
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(scores_array, 
+                xticklabels=np.round(xvals, 2),
+                yticklabels=np.round(yvals, 2),
+                cmap='viridis',
+                annot=True,
+                fmt='.2f',)
+                #cbar_kws={'label': 'Mean Learner Adaptation'})
+
+    # Set labels and title
+    plt.xlabel(xlabel, fontsize=ax_font_size)
+    plt.ylabel(ylabel, fontsize=ax_font_size)
+
+    plt.xticks(rotation=45)
+
+    plt.tight_layout()
+
+    plt.savefig(dname+f"{params_dict['sim_name']}-{plt_tag}.pdf", dpi=300)
